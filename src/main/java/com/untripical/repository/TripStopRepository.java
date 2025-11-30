79446172829fc7b01276a458b924b401f8179e5c
Mateusz <mateusz.kolodziejczyk04@gmail.com>
2025-11-30 22:39:51 +0100

    repaired conflicts

--- src/main/java/com/untripical/repository/TripStopRepository.java
+++ src/main/java/com/untripical/repository/TripStopRepository.java
@@ -10,9 +10,4 @@ import java.util.List;
 @Repository
 public interface TripStopRepository extends JpaRepository<TripStop, Long> {
     List<TripStop> findByTripPlanId(Long tripPlanId);
-    @Query("SELECT COALESCE(MAX(ts.orderIndex), 0) FROM TripStop ts WHERE ts.tripPlan.id = :tripPlanId")
-    int findMaxOrderIndexByTripPlanId(Long tripPlanId);
-    TripStop findByName(String name);
-    boolean existsByNameAndTripPlan_Id(String name, Long tripPlanId);
-
 }
