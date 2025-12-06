6175328d56654dd2a65c36fcdc2e0fb55b973dff
Mateusz <mateusz.kolodziejczyk04@gmail.com>
2025-12-06 11:54:01 +0100

    repaired some bugs

--- src/main/java/com/untripical/security/SecurityConfig.java
+++ src/main/java/com/untripical/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {git
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
     }
 
